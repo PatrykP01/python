3da10256e91bfea3d75f804b49ad43eaf6f09bda
PatrykP01 <patryk.pie01@gmail.com>
2025-04-02 11:48:55 +0200

    .

---
+++ projekt3.py
@@ -0,0 +1,9 @@
+import math
+def nwd(a,b):
+    return math.gcd(a,b)
+def nww(a,b):
+    return abs(a*b) // nwd(a,b)
+a=int(input("Podaj pierwszą liczbę"))
+b=int(input("Podaj pierwszą liczbę"))
+print(f"NWW({a},{b})= {nwd(a,b)}")
+
